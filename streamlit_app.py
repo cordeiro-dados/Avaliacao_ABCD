6ab1d6e2449dd04372f1bb327e46b0985158ff0a
Gabriel Cordeiro <cordeirodados@gmail.com>
2024-09-17 13:35:44 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from login import login_page
 from avaliacao_abcd import abcd_page
+from func_data import func_data_page  # Importar a nova página
 
 # Cria um seletor de páginas na barra lateral
 st.sidebar.title("Navegação")
-pagina_selecionada = st.sidebar.selectbox("Escolha a página", ["Login", "Aplicação ABCD"])
+pagina_selecionada = st.sidebar.selectbox("Escolha a página", ["Login", "Aplicação ABCD", "Funcionários Data"])  # Adicionar nova opção
 
 # Carrega a página selecionada
 if pagina_selecionada == "Login":
     login_page()
 elif pagina_selecionada == "Aplicação ABCD":
     abcd_page()
+elif pagina_selecionada == "Funcionários Data":  # Nova página
+    func_data_page()  # Chama a função da nova página
